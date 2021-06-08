1784d2dfb9ce1ca1e85a4c56627b6013e48b4e2f
Xenosians <fio.salis@gmail.com>
2021-06-08 20:38:49 +0700

    Update SaveScript

--- Assets/Script/SaveScript.cs
+++ Assets/Script/SaveScript.cs
@@ -28,12 +28,18 @@ public class Main : MonoBehaviour
     private void Start(){
   
         SaveScript save = new SaveScript();
-        string json = GDCUtils.ConvertObjectToJsonString<SaveScript>(save);
+        string Ss1 = GDCUtils.ConvertObjectToJsonString<SaveScript>(save);
         save.thisIsFloat = GetTapCountSave();
-        save.time = GetTime();
-        save.thisIsInt = GetHighscore();
+        SaveScript save2 = new SaveScript();
+        string Ss2 = GDCUtils.ConvertObjectToJsonString<SaveScript>(save2);
+        save2.time = GetTime();
+        SaveScript save3 = new SaveScript();
+        string Ss3 = GDCUtils.ConvertObjectToJsonString<SaveScript>(save3);
+        save3.thisIsInt = GetHighscore();
 
-        GDCUtils.SaveFileToLocation(json, "C:/SERBASERBILAB/LAB_RPLGDC/SaveState/Save.json");
+        GDCUtils.SaveFileToLocation(Ss1, "C:/SERBASERBILAB/LAB_RPLGDC/SaveState/Save.json");
+        GDCUtils.SaveFileToLocation(Ss2, "C:/SERBASERBILAB/LAB_RPLGDC/SaveState/Save2.json");
+        GDCUtils.SaveFileToLocation(Ss3, "C:/SERBASERBILAB/LAB_RPLGDC/SaveState/Save3.json");
     }
 
 }
